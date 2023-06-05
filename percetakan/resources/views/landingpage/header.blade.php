327dcb4d508df53d3bbe7f0e54bd3494eae1a4da
Afkar <afkarsiddiq0@gmail.com>
2023-06-05 22:57:21 +0700

    commit

--- percetakan/resources/views/landingpage/header.blade.php
+++ percetakan/resources/views/landingpage/header.blade.php
@@ -22,7 +22,7 @@
                     </ul>
                 </li>
                 <li><a class="nav-link scrollto" href="{{ url('/contact') }}">Contact</a></li>
-                <li><a class="getstarted scrollto" href="{{url('/login')}}">Get Started</a></li>
+                <li><a class="getstarted scrollto" href="{{url('/login')}}">Login</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
