baa4654a4e40b485c92c58d8b4a6986f87c01c5f
Afkar <afkarsiddiq0@gmail.com>
2023-06-15 16:27:21 +0700

    adding level upgrade

---
+++ percetakan/resources/views/user/updatelevel.blade.php
@@ -0,0 +1,51 @@
+@extends('admin.index')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Edit level for') }} {{ $row->name }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('updatelevel.update', $row->id) }}">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="form-group">
+                            <label for="level">Member Level</label>
+                            <select id="level" name="level" class="form-control">
+                                <option value="admin" {{ $row->level === 'admin' ? 'selected' : '' }}>admin</option>
+                                <option value="manajer" {{ $row->level === 'manajer' ? 'selected' : '' }}>manajer</option>
+                                <option value="kasir" {{ $row->level === 'kasir' ? 'selected' : '' }}>kasir</option>
+                            </select>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary">Update Level</button>
+                    </form>
+
+
+                    @if(Session::has('success'))
+                    <script>
+                        $(document).ready(function() {
+                            alert("{{ Session::get('success') }}");
+                        });
+                    </script>
+                    @endif
+
+                    @if(Session::has('error'))
+                    <script>
+                        $(document).ready(function() {
+                            alert("{{ Session::get('error') }}");
+                        });
+                    </script>
+                    @endif
+
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
