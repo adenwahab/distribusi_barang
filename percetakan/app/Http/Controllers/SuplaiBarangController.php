3a9984f22e4db2448e48e1ce47dde98f6e5ae920
Afkar <afkarsiddiq0@gmail.com>
2023-06-21 02:01:51 +0700

    fix suplai barang

--- percetakan/app/Http/Controllers/SuplaiBarangController.php
+++ percetakan/app/Http/Controllers/SuplaiBarangController.php
@@ -44,7 +44,7 @@ class SuplaiBarangController extends Controller
                 'suplier' => 'required',
                 'jumlah' => 'required|integer',
                 'date' => 'required|date',
-                'kode'  => '',
+                // 'kode'  => '',
             ],
             //custom pesan errornya
             [
@@ -56,7 +56,7 @@ class SuplaiBarangController extends Controller
         $selectedBarang = $request->input('barang');
 
         $dataBarang = explode(' | ', $selectedBarang);
-        $idBarang = $dataBarang[3];
+        $idBarang = $dataBarang[1];
 
         $selectedSuplier = $request->input('suplier');
         $dataSuplier = explode(' | ', $selectedSuplier);
