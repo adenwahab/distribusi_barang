8d8ecef159034b3eec50f11776f593ddfb670dc8
alamcahyo <alamcahyo3@gmail.com>
2023-06-21 13:22:01 +0700

    nambah button di home dan nama univ

--- percetakan/resources/views/landingpage/services.blade.php
+++ percetakan/resources/views/landingpage/services.blade.php
@@ -13,7 +13,7 @@
               <div class="container px-4 px-lg-5 mt-5">
                   <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                       <div class="col mb-5">
-                          <div class="card h-100">
+                          <div class="card h-100 shadow ">
                               <!-- Product image-->
                               <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                               <!-- Product details-->
@@ -32,7 +32,7 @@
                           </div>
                       </div>
                       <div class="col mb-5">
-                          <div class="card h-100">
+                          <div class="card h-100 shadow ">
                               <!-- Sale badge-->
                               <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
                               <!-- Product image-->
@@ -62,7 +62,7 @@
                           </div>
                       </div>
                       <div class="col mb-5">
-                          <div class="card h-100">
+                          <div class="card h-100 shadow">
                               <!-- Sale badge-->
                               <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
                               <!-- Product image-->
@@ -84,7 +84,7 @@
                           </div>
                       </div>
                       <div class="col mb-5">
-                          <div class="card h-100">
+                          <div class="card h-100 shadow">
                               <!-- Product image-->
                               <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                               <!-- Product details-->
@@ -111,7 +111,7 @@
                           </div>
                       </div>
                       <div class="col mb-5">
-                          <div class="card h-100">
+                          <div class="card h-100 shadow">
                               <!-- Sale badge-->
                               <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
                               <!-- Product image-->
@@ -133,7 +133,7 @@
                           </div>
                       </div>
                       <div class="col mb-5">
-                          <div class="card h-100">
+                          <div class="card h-100 shadow">
                               <!-- Product image-->
                               <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                               <!-- Product details-->
@@ -152,7 +152,7 @@
                           </div>
                       </div>
                       <div class="col mb-5">
-                          <div class="card h-100">
+                          <div class="card h-100 shadow">
                               <!-- Sale badge-->
                               <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
                               <!-- Product image-->
@@ -182,7 +182,7 @@
                           </div>
                       </div>
                       <div class="col mb-5">
-                          <div class="card h-100">
+                          <div class="card h-100 shadow ">
                               <!-- Product image-->
                               <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                               <!-- Product details-->
@@ -208,6 +208,7 @@
                               </div>
                           </div>
                       </div>
+                      <button type="button" class="btn btn-link">Lihat lebih banyak</button>
                   </div>
               </div>
           </section>
